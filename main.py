12eac32d79de45347dcbb0d94af9897fbc46e1d6
jpayne0061 <jpayne0061@gmail.com>
2020-01-05 08:59:41 -0500

    clean up code

--- main.py
+++ main.py
@@ -1,53 +1,40 @@
 from flask import Flask
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
+import RPi.GPIO as GPIO
+from time import sleep
 from flask import render_template
 from flask import request
 import random
 from flask import abort
 
+PIN_ELEVEN = 11
+PIN_FIVE = 5
+PIN_THREE = 3
+
+ALL_PINS = [PIN_ELEVEN, PIN_FIVE, PIN_THREE]
+
+CURRENT_SEQUENCE = []
+
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-
-all_pins = [11,5,3]
-
-current_sequence = []
+GPIO.setup(PIN_ELEVEN, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(PIN_FIVE, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(PIN_THREE, GPIO.OUT, initial=GPIO.LOW) 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.route('/index')
+@app.route('/')
 def signUp():
     return render_template('index.html')
-
-@app.route('/light-led', methods=['POST'])
-def light_up():
-    pin = request.args.get('pin')
-    pin = int(pin)
-    for x in all_pins:
-        if(x != pin):
-            GPIO.output(x, GPIO.LOW)
-
-    GPIO.output(pin, GPIO.HIGH) # Turn on
-    return ""
 
 @app.route('/check-sequence', methods=['POST'])
 def check_sequence():
     user_sequence = request.args.get('sequence')
-    print('user sequence: ' + user_sequence)
-    print('current_sequence')
-    print(current_sequence)
     user_sequence = user_sequence.split(',')
     user_sequence = [int(i) for i in user_sequence]
-    for i in range(0, len(current_sequence)):
+
+    for i in range(0, len(CURRENT_SEQUENCE)):
         try:
-            if(user_sequence[i] != current_sequence[i]):
+            if(user_sequence[i] != CURRENT_SEQUENCE[i]):
                 wrong_answer()
                 start_new()
                 return "fail"
@@ -62,43 +49,42 @@ def check_sequence():
     return ""
 
 def wrong_answer():
-    for i in range(0, 5):
-        GPIO.output(11, GPIO.HIGH)
-        GPIO.output(5, GPIO.HIGH)
-        GPIO.output(3, GPIO.HIGH)
+    for _i in range(0, 5):
+        GPIO.output(PIN_ELEVEN, GPIO.HIGH)
+        GPIO.output(PIN_FIVE, GPIO.HIGH)
+        GPIO.output(PIN_THREE, GPIO.HIGH)
         sleep(0.125)
-        GPIO.output(11, GPIO.LOW)
-        GPIO.output(5, GPIO.LOW)
-        GPIO.output(3, GPIO.LOW)
+        GPIO.output(PIN_ELEVEN, GPIO.LOW)
+        GPIO.output(PIN_FIVE, GPIO.LOW)
+        GPIO.output(PIN_THREE, GPIO.LOW)
         sleep(0.125)
 
 def start_new():
-    global current_sequence
-    current_sequence = []
+    global CURRENT_SEQUENCE
+    CURRENT_SEQUENCE = []
     rand_pin = gen_random_pin()
-    current_sequence.append(rand_pin)
+    CURRENT_SEQUENCE.append(rand_pin)
     GPIO.output(rand_pin, GPIO.HIGH) # Turn on
     sleep(0.5)
     GPIO.output(rand_pin, GPIO.LOW) # Turn on
 
 def add_to_sequence():
     rand_pin = gen_random_pin()
-    current_sequence.append(rand_pin)
+    CURRENT_SEQUENCE.append(rand_pin)
 
 def all_pins_low():
-    for p in all_pins:
+    for p in ALL_PINS:
         GPIO.output(p, GPIO.LOW)
 
 def display_current_sequence():
-    for p in current_sequence:
+    for p in CURRENT_SEQUENCE:
         GPIO.output(p, GPIO.HIGH)
         sleep(0.3)
         GPIO.output(p, GPIO.LOW)
         sleep(0.3)
 
-
 def gen_random_pin():
-    return all_pins[random.randint(0,2)]
+    return ALL_PINS[random.randint(0,2)]
 
 if __name__ == "__main__":
     start_new()
